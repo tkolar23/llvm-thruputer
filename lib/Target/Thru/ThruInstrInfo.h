82029e73b6a1de8ee6a569423bb162d30442d3f5
ChrisMMorrison <chris.morrison0212@gmail.com>
2022-02-08 13:42:43 -0500

    Added ThruInstrInfo class files and added instr info table gen files and classes to cmake.

---
+++ lib/Target/Thru/ThruInstrInfo.h
@@ -0,0 +1,18 @@
+//
+// Created by Chris on 2/8/2022.
+//
+
+#ifndef LLVM_LIB_TARGET_THRU_THRUINSTRINFO_H
+#define LLVM_LIB_TARGET_THRU_THRUINSTRINFO_H
+
+//#include "ThruRegisterInfo.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
+
+#define GET_INSTRINFO_HEADER
+//#include "ThruGenInstrInfo.inc"
+
+namespace llvm {
+
+}
+
+#endif //LLVM_LIB_TARGET_THRU_THRUINSTRINFO_H
